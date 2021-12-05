f2a8d501bb073a928387fb15f43d160baab493d1
Naresh M <mnaresh.y@gmail.com>
2021-12-05 23:31:50 +0530

    URs update

--- src/main/java/com/apigateway/we/APIGateWayServer.java
+++ src/main/java/com/apigateway/we/APIGateWayServer.java
@@ -7,6 +7,23 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class APIGateWayServer {
 
+
+    /*
+    * URLS
+    *
+    *http://localhost:8765/MSINSURENCEWE/quote/premium/feign/NareshM/KA51MC9384
+    *
+    * http://localhost:8765/   -  API GATEWAY
+    *
+    * MSINSURENCEWE - app name registered in Eureka
+    *
+    * quote/premium/feign/NareshM/KA51MC9384 - service URL
+    *
+    *
+    * http://localhost:8765/QUOTE-SERVICE/quote/premium/feign/NareshM/KA51MC9384
+    *
+    * */
+
     public static void main(String[] args) {
 
         SpringApplication.run(APIGateWayServer.class);

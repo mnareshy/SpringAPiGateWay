60fcdeb0078342fe491e0f2f059d36da5c3314ce
Naresh M <mnaresh.y@gmail.com>
2021-12-05 13:57:56 +0530

    Initial commit

---
+++ src/main/java/com/apigateway/we/APIGateWayServer.java
@@ -0,0 +1,15 @@
+package com.apigateway.we;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APIGateWayServer {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(APIGateWayServer.class);
+
+    }
+}
